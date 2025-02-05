47ef49247ac1c11713dfae2fe44f48b1f9902c6c
Yingle <3572399126@qq.com>
2025-02-05 21:49:10 +0800

    删除直行、左右转标志，添加自主部分结束标志。测试成功

--- main.c
+++ main.c
@@ -7,33 +7,31 @@ sbit SENSOR = P2^7;  // 传感器连接 P27
 
 // 主函数开始
 void main() {
-    unsigned int flag_ball = 1;// 进球标志
+    char flag_ball = 1;// 进球标志
 
-    Motor_Init();// 初始化马达
+    Motor_Init();// 初始化电机
     Timer0Init();// 初始化定时器
 
     // 主循环
 	while (1) {
 		if (flag_ball == 1) {
 			if (SENSOR == 0)
-				flag_ball = 0;
+				flag_ball = 0;// 触发进球行为
 			else
-				Motor_Forward(3, 3);
-			// 电机直行
+				Motor_Forward(3, 3);// 电机直行
 		} 
 
 		if (flag_ball == 0){
-			// 若进球，关门、暂停、右转、直行至安全区
+			flag_ball = -1;// 自主部分结束
+			// 若进球，暂停、右转、直行至安全区
       		// Door_Close();
        		Motor_TempStop(1000);
        		Motor_TurnRight(3, 3);
 
 			Motor_TempStop(500);
 			Motor_TempForward(3000, 3, 3); //直行3秒
-			// Motor_Stop();
 		}
 
-    	DelayMs(100);  // 每 100ms 循环一次
     	DelayMs(100);  // 每 100ms 循环一次
     }
 }
